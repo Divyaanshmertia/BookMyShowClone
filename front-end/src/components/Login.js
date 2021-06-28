ed8434e73555dcbccfac4a67ed066836a755afbc
Divyaansh <divyaanshmertia@gmail.com>
2021-06-28 18:05:37 +0530

    Login now working

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,75 +1,68 @@
-import React from "react";
-import "../stylesheets/style.css";
+import React, { useState } from "react";
 import Axios from "axios";
-class Login extends React.Component {
-	state = { credentials: {} };
+import { useHistory } from "react-router-dom";
 
-	onSubmitClick = async (event) => {
-		event.preventDefault();
-		console.log(this.state.credentials);
-		await Axios.post("http://localhost:9160/login", this.state.credentials)
-			.then(({ data }) => {
-				localStorage.setItem("user", JSON.stringify(data));
-				this.props.setUserState(data.user);
-				console.info(data);
-			})
-			.catch((error) => {
-				console.error(error);
-			})
-			.finally(() => {
-				console.info("Login API called");
-			});
-	};
-	render() {
-		return (
-			<div className={"section"}>
-				<form className={"ui form"}>
-					<h4 className={"ui dividing header"}>Login</h4>
-					<div className={"field"}>
-						<div className={"two-fields"}>
-							<label>e-mail</label>
-							<div className={"field"}>
-								<input
-									type={"email"}
-									placeholder={"Email *"}
-									onChange={(e) => {
-										this.setState({
-											credentials: {
-												...this.state.credentials,
-												email: e.target.value,
-											},
-										});
-									}}
-								/>
-							</div>
-							<label>Password</label>
-							<div className={"field"}>
-								<input
-									type={"password"}
-									placeholder={"Password *"}
-									onChange={(e) => {
-										this.setState({
-											credentials: {
-												...this.state.credentials,
-												passWord: e.target.value,
-											},
-										});
-									}}
-								/>
-							</div>
-						</div>
-					</div>
-					<button
-						className={"ui primary button"}
-						onClick={this.onSubmitClick}
-						style={{ backgroundColor: "#d84c4c" }}
-					>
-						Submit
-					</button>
-				</form>
-			</div>
-		);
-	}
-}
+const Login = (props) => {
+  const { setUserState } = props;
+  const history = useHistory();
+  const [credentials, setCredentials] = useState({});
+
+  const onSubmitClick = async (event) => {
+    event.preventDefault();
+    await Axios.post("http://localhost:9160/login", credentials)
+      .then(({ data }) => {
+        console.info(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        setUserState(data.user);
+        history.push("/");
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        console.info("Login API call finished.");
+      });
+  };
+
+  return (
+    <div className={"section"}>
+      <form className={"ui form"}>
+        <h4 className={"ui dividing header"}>Login</h4>
+        <div className={"field"}>
+          <label>Email</label>
+          <div className={"two-fields"}>
+            <div className={"field"}>
+              <input
+                type={"email"}
+                placeholder={"Email *"}
+                onChange={(e) => {
+                  setCredentials({
+                    ...credentials,
+                    email: e.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className={"field"}>
+              <input
+                type={"password"}
+                placeholder={"Password *"}
+                onChange={(e) => {
+                  setCredentials({
+                    ...credentials,
+                    password: e.target.value,
+                  });
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <button className={"ui primary button"} onClick={onSubmitClick}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;

eacafcc3c12cbaf7eedc4c90181ed108c80f6bf6
Divyaansh Mertia <divyaanshmertia@gmail.com>
2021-06-28 18:35:48 +0530

    add app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,64 +12,68 @@ import Home from "./components/home";
 import Error from "./components/error";
 import ShowCards1 from "./components/MovieCards";
 import Home1 from "./components/home1";
+import CustomPage from "./components/CustomPage";
 
 class App extends React.Component {
-	state = {
-		user: {},
-	};
-	componentDidMount() {
-		let savedUSer = localStorage.getItem("user");
-		if (savedUSer && Object.keys(this.state.user).length === 0) {
-			console.log("Login Identified");
-			this.setUserState(JSON.parse(savedUSer).user);
-		}
-	}
+  state = {
+    user: {},
+  };
+  componentDidMount() {
+    let savedUSer = localStorage.getItem("user");
+    if (savedUSer && Object.keys(this.state.user).length === 0) {
+      console.log("Login Identified");
+      this.setUserState(JSON.parse(savedUSer).user);
+    }
+  }
 
-	setUserState = (user) => {
-		this.setState({ user: user });
-	};
+  setUserState = (user) => {
+    this.setState({ user: user });
+  };
 
-	render() {
-		return (
-			<Router>
-				<div>
-					<Header />
-					<Switch>
-						<Route path={"/"} exact>
-							<Home />
-							<ShowCards />
-						</Route>
-						<Route path={"/login"}>
-							<Login setUserState={this.setUserState} />
-						</Route>
-						<Route path={"/selectSeat"}>
-							<HallForm />
-						</Route>
-						<Route path={"/signup"}>
-							<SignUp setUserState={this.setUserState} />
-						</Route>
-						<Route path={"/about-us"}>
-							<h1>This is contact-us </h1>
-						</Route>
-						<Route path={"/movies"}>
-							<Home1 />
-							<ShowCards1 />
-						</Route>
-						<Route path={"/contact-us"}></Route>
-						<Route path={"/error"}>
-							<Error />
-						</Route>
-						<Route path={"/**"}>
-							<Redirect to={"/error"} />
-						</Route>
-						<Route path={"/location"}>
-							<pan>ON CONSTRUCTION</pan>
-						</Route>
-					</Switch>
-					<Footer />
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route path={"/"} exact>
+              <Home />
+              <ShowCards />
+            </Route>
+            <Route path={"/login"}>
+              <Login setUserState={this.setUserState} />
+            </Route>
+            <Route path={"/signup"}>
+              <SignUp setUserState={this.setUserState} />
+            </Route>
+            <Route path={"/about-us"}>
+              <h1>This is contact-us </h1>
+            </Route>
+            <Route path={"/movies"}>
+              <Home1 />
+              <ShowCards1 />
+            </Route>
+            <Route path={"/movie/:id"}>
+              <CustomPage />
+            </Route>
+            <Route path={"/movieseat/:id"}>
+              <HallForm />
+            </Route>
+            <Route path={"/contact-us"}></Route>
+            <Route path={"/error"}>
+              <Error />
+            </Route>
+            <Route path={"/**"}>
+              <Redirect to={"/error"} />
+            </Route>
+            <Route path={"/location"}>
+              <pan>ON CONSTRUCTION</pan>
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 export default App;
